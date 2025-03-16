8443ec77ea802d9ccc090adc6184abddc8a208ab
Joel Godoy <j.godoy.moraes@gmail.com>
2025-03-16 09:36:51 -0300

    Confusion Matrix

--- deteccao_fraude.py
+++ deteccao_fraude.py
@@ -29,3 +29,40 @@ df=pd.get_dummies(data=df,columns=['type'])
 y = df['isFraud']
 
 x = df.drop(['isFraud'],axis=1)
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(max_iter=300, random_state=42)
+
+lr.fit(x_train,y_train)
+
+y_pred=lr.predict(x_test)
+
+#%%
+
+#Evaluating the logist regression model
+
+from sklearn import metrics
+
+print("LR Accuracy: ",metrics.accuracy_score(y_test, y_pred))
+print("LR Precision: ",metrics.precision_score(y_test, y_pred))
+print("LR Recall: ",metrics.recall_score(y_test, y_pred))
+print("LR F1-score: ",metrics.f1_score(y_test, y_pred))
+
+# General accuracy is pretty good, but the oother metrics are not fine
+
+# Lets evaluate the confusion matrix
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+cm= confusion_matrix(y_test,y_pred)
+display = ConfusionMatrixDisplay(confusion_matrix=cm)
+display.plot()
+
+# We see that the true positive score is terrible, probably due to unbalanced data
